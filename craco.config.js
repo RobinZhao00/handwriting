de619fe43ea45771ce1bd3ed2186edba75260f62
RobinZhao00 <yuzhao.ecust@gmail.com>
2025-04-17 16:22:25 +0800

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,14 @@
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      if (env === 'production') {
+        // 设置 publicPath 为生产环境的路径
+        webpackConfig.output.publicPath = 'https://latteai.com/handwriting/';
+      }
+      return webpackConfig;
+    },
+  },
   style: {
     postcss: {
       mode: 'file',
